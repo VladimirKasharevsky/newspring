6fa089888c419d0cbcd1f0dd4c360291a770ffd7
Vladimir Kasharevskiy <kashar015@rambler.ru>
2019-10-20 18:31:49 +0400

    Реализован редирект

--- src/main/java/org/mentor/spring/controller/MyController.java
+++ src/main/java/org/mentor/spring/controller/MyController.java
@@ -2,16 +2,12 @@ package org.mentor.spring.controller;
 
 import org.mentor.spring.model.User;
 import org.mentor.spring.service.UserService;
-import org.mentor.spring.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.stereotype.Controller;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @Controller
@@ -37,14 +33,14 @@ public class MyController {
     @RequestMapping (value = "/admin/delete", method = RequestMethod.POST)
     public ModelAndView  deleteUser(@RequestParam String id) {
         userService.deleteUser(id);
-        return userForm();
+        return new ModelAndView("redirect:/");
     }
 
     @RequestMapping (value = "/admin/create", method = RequestMethod.POST)
     public ModelAndView  createUser(@RequestParam String name, @RequestParam String pass, @RequestParam String role) {
         User user = new User(name, pass, role);
         userService.createUser(user);
-        return userForm();
+        return new ModelAndView("redirect:/");
     }
 
     @RequestMapping (value = "/admin/update", method = RequestMethod.GET)
@@ -61,9 +57,8 @@ public class MyController {
 
     @RequestMapping (value = "/admin/update", method = RequestMethod.POST)
     public ModelAndView  updateUser(@RequestParam Long id, @RequestParam String newName, @RequestParam String newPassword, @RequestParam String role) {
-        ModelAndView modelAndView = new ModelAndView();
         User user = new User(id, newName, newPassword, role);
         userService.updateUser(user);
-        return modelAndView;
+        return new ModelAndView("redirect:/");
     }
 }
